b793d266f43c39c2b9ded6b3b951e6945b6ba3eb
sadbatya <sadbatya@mail.ru>
2024-03-12 18:27:27 -0400

    fix png

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -8,8 +8,8 @@ export default function StartPage() {
   return (
     <div className="h-screen relative">
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-      <div className="bg-white"><img src="/public/quiz.png" className="w-20 h-22 mx-auto mb-2 animate-bounce"/></div>
-        <h1 className="text-center mb-5 text-4xl">Quiz 0.asdasdasdsdas1</h1>
+      <div className="bg-white"><img src="/quiz.png" className="w-20 h-22 mx-auto mb-2 animate-bounce"/></div>
+        <h1 className="text-center mb-5 text-4xl">Quiz 0.1</h1>
         <div className="flex gap-5">
           <Button text={'Start Test'} onClick={() => navigate('/test')}/>
           <Button text={'Edit Test'} onClick={() => navigate('/edit')}/>
