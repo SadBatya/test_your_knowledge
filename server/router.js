e6270a1a3bc636e1a4dc986e69502406fdb21786
sadbatya <sadbatya@mail.ru>
2024-03-20 10:01:28 -0400

    upload frontend

--- server/router.js
+++
@@ -1,9 +0,0 @@
-// router.js
-import express from 'express';
-import { getQuestion } from './getQuestion';
-
-const router = express.Router();
-
-router.get('/question', getQuestion);
-
-export default router;
